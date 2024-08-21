a3ac3bdb4c07260775ff2ae3796970d64f713665
Justin W <41334202+gpzj@users.noreply.github.com>
2024-08-21 15:40:14 -0400

    Create bllockscreen.sh

---
+++ scripts/bllockscreen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script for monitoring lock/unlock screen of Gnome session
+dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
+  while read x; do
+    case "$x" in 
+      *"boolean true"*) curl -s http://localhost:8000/light/0/on?color=yellow > /dev/null;;
+      *"boolean false"*) curl -s http://localhost:8000/light/0/on?color=green > /dev/null;;  
+    esac
+  done
