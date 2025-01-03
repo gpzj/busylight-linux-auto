4b6008a8c0a5f6998422ba23f46cd55c9babd38b
Justin W <41334202+gpzj@users.noreply.github.com>
2025-01-03 14:37:13 -0500

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+alias blgreen='curl -s http://localhost:8000/light/0/on?color=green > /dev/null'
+alias blblue='curl -s http://localhost:8000/light/0/on?color=blue > /dev/null'
+alias blred='curl -s http://localhost:8000/light/0/on?color=red > /dev/null'
+alias blpurple='curl -s http://localhost:8000/light/0/on?color=purple > /dev/null'
+alias blyellow='curl -s http://localhost:8000/light/0/on?color=yellow > /dev/null'
+alias blorange='curl -s http://localhost:8000/light/0/on?color=orange > /dev/null'
+alias blpink='curl -s http://localhost:8000/light/0/on?color=pink > /dev/null'
+alias blwhite='curl -s http://localhost:8000/light/0/on?color=white > /dev/null'
